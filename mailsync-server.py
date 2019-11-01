d3e49d421c04039755ae90ef139f90978817d6f0
surarim <surarim@yandex.ru>
2019-11-01 13:50:05 +0700

    Small fixes and code review

--- mailsync-server.py
+++ mailsync-server.py
@@ -6,7 +6,6 @@
 
 from datetime import datetime
 import os, psycopg2, paramiko
-
 config = [] # Список параметров файла конфигурации
 
 #------------------------------------------------------------------------------------------------
@@ -24,12 +23,13 @@ def get_config(key):
         configfile = open('mailsync.conf')
     except IOError as error:
       log_write(error)
-    for line in configfile:
-      param = line.partition('=')[::2]
-      if param[0].strip().isalpha() and param[0].strip().find('#') == -1:
-        # Получение параметра
-        config.append(param[0].strip())
-        config.append(param[1].strip())
+    else:
+      for line in configfile:
+        param = line.partition('=')[::2]
+        if param[0].strip().isalpha() and param[0].strip().find('#') == -1:
+          # Получение параметра
+          config.append(param[0].strip())
+          config.append(param[1].strip())
   try:
     result = config[config.index(key)+1]
   except ValueError as err:
@@ -64,42 +64,38 @@ def log_write(message):
 #------------------------------------------------------------------------------------------------
 
 def run():
-  # Получение списка почтовых ящиков
+  # Получение списка почтовых ящиков из zimbra
   ssh = paramiko.SSHClient()
   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
   log_write('Sync mailboxes...')
   ssh.connect(hostname=get_config('ZimbraHost'), username=get_config('ZimbraUser'), password=get_config('ZimbraPasswd'))
   stdin, stdout, stderr = ssh.exec_command('su - zimbra -c "zmprov -l gaa"')
-  mailbox_zimbra = (stdout.read() + stderr.read()).decode()
+  mailboxes_zimbra = (stdout.read() + stderr.read()).decode()
   ssh.close()
-
   # Чтение текущих ящиков на posfix
   conn = psycopg2.connect("host='localhost' dbname="+get_config('MailDatabase')+" user="+get_config('MailUser')+" password="+get_config('MailPasswd'))
   cursor = conn.cursor()
   cursor.execute("select * from relay_recipient;")
-  mailbox_local=''
+  mailboxes_local=''
   for row in cursor:
-    mailbox_local += row[0]+'\n'
-
+    mailboxes_local += row[0]+'\n'
   # Чтение таблицы локальных исключений
   cursor.execute("select * from mailboxes_localonly;")
   mailboxes_localonly=''
   for row in cursor:
     mailboxes_localonly += row[0]+'\n'
-
   # Удаление локальных ящиков, которые более не присутствуют на zimbra
-  for line in mailbox_local.splitlines():
-    if mailbox_zimbra.find(line) == -1 or line in mailboxes_localonly:
+  for line in mailboxes_local.splitlines():
+    if mailboxes_zimbra.find(line) == -1 or line in mailboxes_localonly:
       try:
         cursor.execute("delete from relay_recipient where mailbox like %s;",(line,))
       except psycopg2.Error as error:
         print(format(error))
         exit(1)
       log_write('Deleted '+line)
-
   # Добавление новых локальных ящиков, которые есть на zimbra
-  for line in mailbox_zimbra.splitlines():
-    if mailbox_local.find(line) == -1 and line not in mailboxes_localonly:
+  for line in mailboxes_zimbra.splitlines():
+    if mailboxes_local.find(line) == -1 and line not in mailboxes_localonly:
       try:
         cursor.execute("insert into relay_recipient values (%s);",(line,))
       except psycopg2.Error as error:
